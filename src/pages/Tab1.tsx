9f74b230a69b09c2db1b9732145042b0b4da5ea7
radek79 <radek79@gmail.com>
2022-03-30 18:10:45 -0700

    recreated even with two bar charts

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -6,56 +6,24 @@ import Chart from "react-apexcharts";
 
 const Tab1: React.FC = () => {
 
-  const opts: any = {
-    plotOptions: {
-      radialBar: {
-        hollow: {
-          margin: 0,
-          size: "75%"
-        },
-        track: {
-          show: true,
-          opacity: 0.2,
-          dropShadow: {
-            enabled: true,
-            top: 2,
-            left: 0,
-            blur: 4,
-            opacity: 0.15
-          }
-        },
-        dataLabels: {
-          name: {
-            offsetY: 50,
-            fontSize: "13px"
-          },
-          value: {
-            fontSize: "50px",
-            fontWeight: 600,
-            offsetY: 0,
-            show: true,
-          }
-        }
+  const initialData = {
+    options: {
+      chart: {
+        id: "basic-bar"
+      },
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
       }
     },
-    fill: {
-      type: "gradient",
-      gradient: {
-        shade: "dark",
-        shadeIntensity: 1,
-        type: "horizontal",
-        stops: [0, 100],
-        opacityFrom: 1,
-        opacityTo: 1
+    series: [
+      {
+        name: "series-1",
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
       }
-    },
-    stroke: {
-      lineCap: "round" as const
-    },
-    labels: ["remaining"]
+    ]
   };
 
-  const [currentState, setState] = useState([70]);
+  const [currentState, setState] = useState(initialData);
 
   return (
     <IonPage>
@@ -70,10 +38,7 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <Chart options={opts}
-        series={[70]}
-        type="radialBar"
-        width="100%"/>
+        <Chart options={currentState.options} series={currentState.series} type="bar" />
       </IonContent>
     </IonPage>
   );
